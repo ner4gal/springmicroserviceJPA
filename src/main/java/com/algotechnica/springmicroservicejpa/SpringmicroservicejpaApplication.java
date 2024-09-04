dc5e0fa55cd34ba6553e3daaa4381cdfff672ed7
ner4gal <karimelalkaoui@gmail.com>
2024-09-04 18:53:32 +0100

    Initial commit

---
+++ src/main/java/com/algotechnica/springmicroservicejpa/SpringmicroservicejpaApplication.java
@@ -0,0 +1,13 @@
+package com.algotechnica.springmicroservicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicroservicejpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algotechnica/springmicroservicejpa/SpringmicroservicejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algotechnica.springmicroservicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservicejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
